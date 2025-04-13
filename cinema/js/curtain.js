d3e1d60c43c70bc718aeb7a93e2121bc0d80cf11
Calvin Josenhans <calvinjosenhans10@gmail.com>
2025-04-13 14:29:08 -0400

    asdfsdaf

--- cinema/js/curtain.js
+++ cinema/js/curtain.js
@@ -2,15 +2,21 @@ document.addEventListener('DOMContentLoaded', function() {
     const leftCurtain = document.querySelector('.left-curtain');
     const rightCurtain = document.querySelector('.right-curtain');
 
+    let max_scroll = 0
+    const scrollDist = rightCurtain.innerHeight;
     // Function to handle scroll animation
     function handleScroll() {
         const scrollPosition = window.scrollY;
         const windowHeight = window.innerHeight;
-        const scrollPercentage = Math.min(scrollPosition / (windowHeight * 0.5), 1);
+        const scrollPercentage = Math.min(scrollPosition / (windowHeight * 0.25), 1);
+
+        max_scroll = Math.max(scrollPercentage, max_scroll)
+
+        
 
         // Apply transform based on scroll percentage
-        leftCurtain.style.transform = `translateX(${-100 * scrollPercentage}%)`;
-        rightCurtain.style.transform = `translateX(${100 * scrollPercentage}%)`;
+        leftCurtain.style.transform = `translateX(${-100 * max_scroll}%)`;
+        rightCurtain.style.transform = `translateX(${100 * max_scroll}%)`;
     }
 
     // Initial call to set starting position
