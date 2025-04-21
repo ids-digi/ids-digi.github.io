49aacecd66196290fb354a22bc8c6200c3286dcd
Calvin Josenhans <calvinjosenhans10@gmail.com>
2025-04-21 10:44:35 -0400

    updates

--- the-lone-mechanic/js/metadata.js
+++ the-lone-mechanic/js/metadata.js
@@ -90,7 +90,7 @@ document.querySelector('#bylines').innerHTML = bylines_html;
 document.querySelector('.author-bios').innerHTML = bios_html;
 
 // pubdate
-document.querySelector('#pubdate').innerHTML = "Published December 11, 2024 " + data.pub_date;
+document.querySelector('#pubdate').innerHTML = "Published " + data.pub_date;
 
 // title & slug
 document.querySelector('title').innerHTML = data.title + ' | Indiana Daily Student';
